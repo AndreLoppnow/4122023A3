bb0c494530dbbcea68363beb6352dbd7e2cb53ce
André <andref_lloppnow@outlook.com>
2023-12-03 12:36:19 -0300

    Update codigoResolvidoA3.java

--- codigoResolvidoA3.java
+++ codigoResolvidoA3.java
@@ -1,80 +1,87 @@
 import java.util.Scanner;
 
 public class codigoResolvidoA3 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        try {
+    public enum tipoOperacao {
+        SOMAR,
+        SUBTRAIR,
+        MULTIPLICAR,
+        DIVIDIR;
+    };
+    
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Digite o primeiro valor binário: ");
             String valor1 = scanner.next();
 
             System.out.print("Digite o segundo valor binário: ");
             String valor2 = scanner.next();
 
-            int resultado = 0;
+            char[] binario = {'0', '1'};
 
-            String[] tipoOperacao = { "SOMAR", "SUBTRAIR", "MULTIPLICAR", "DIVIDIR" };
-            String[] arrayNoBinario = {"2", "3", "4", "5", "6", "7", "8", "9"};
-            String[] arrayBinario = {"0", "1"};
+            if (Valida(valor1, binario) && Valida(valor2, binario)) {
+                System.out.print("Escolha uma operação: \n");
 
-            for (int i = 0; i < arrayBinario.length; i++) {
-                for (int j = 0; j < arrayNoBinario.length; j++) {
-
-                    if (valor1.contains(arrayBinario[i]) && valor2.contains(arrayBinario[i])) {
-                        if (!valor1.contains(arrayNoBinario[j]) && !valor2.contains(arrayNoBinario[j])) {
-
-                            System.out.print("Escolha uma operação: \n");
-                            int count = 0;
-                            for (String item : tipoOperacao) {
-                                System.out.println("[" + (count + 1) + "]" + item);
-                                count++;
-                            }
-                            int operacao = scanner.nextInt();
-                            if (operacao == 1) {
-                                resultado = Integer.parseInt(valor1, 2) + Integer.parseInt(valor2, 2);
-                            } else if (operacao == 2) {
-                                resultado = Integer.parseInt(valor1, 2) - Integer.parseInt(valor2, 2);
-                            } else if (operacao == 3) {
-                                resultado = Integer.parseInt(valor1, 2) * Integer.parseInt(valor2, 2);
-                            } else if (operacao == 4) {
-                                resultado = Integer.parseInt(valor1, 2) / Integer.parseInt(valor2, 2);
-                            } else {
-                                System.out.println("Operação inválida");
-                            }
-                        } else {
-                            System.out.println("Valores não são binários");
-                            break;
-                        }
-                        break;
-                    } else {
-                        System.out.println("Valores não são binários");
-                    }
-                    break;
+                for(tipoOperacao op : tipoOperacao.values()){
+                    System.out.println("[" + op.ordinal() + "]" + op.name());
                 }
-                break;
-            }
 
-            System.out.println("Resultado binário: " + Integer.toBinaryString(resultado));
+                tipoOperacao operacao = getOperacao(scanner);
+
+                if (operacao == tipoOperacao.DIVIDIR && Integer.parseInt(valor2, 2) == 0) {
+                    System.out.println("Erro: Divisão por zero");
+                } else {
+                    realizarOperacao(operacao, valor1, valor2);
+                };
+ 
+            } else {
+                System.out.println("Erro: Valores não são binários");
+            }
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
         }
     }
 
-    public static Boolean PercorreArray (String[] naoBinario, String[] binario, String valor1, String valor2){
-        for (int i = 0; i < naoBinario.length; i++) {
-            if(valor1.contains(naoBinario[i]) || valor2.contains(naoBinario[i])){
-                return true;
-            }else{
-            }
-            return false;
+    public static tipoOperacao getOperacao(Scanner scan){
+        try{
+            return tipoOperacao.values()[scan.nextInt()];
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Erro: Operação inválida! Digite outro valor:");
+            return getOperacao(scan);
         }
+    }
 
-        for (int i = 0; i < binario.length; i++) {
-            if(valor1.contains(binario[i]) || valor2.contains(naoBinario[i])){
-                return true;
-            }else{
+    public static void realizarOperacao(tipoOperacao operacao, String valor1, String valor2) {
+        int resultado = 0;
+        switch (operacao) {
+            case SOMAR:
+                resultado = Integer.parseInt(valor1, 2) + Integer.parseInt(valor2, 2);
+                break;
+            case SUBTRAIR:
+                resultado = Integer.parseInt(valor1, 2) - Integer.parseInt(valor2, 2);
+                break;
+            case MULTIPLICAR:
+                resultado = Integer.parseInt(valor1, 2) * Integer.parseInt(valor2, 2);
+                break;
+            case DIVIDIR:
+                resultado = Integer.parseInt(valor1, 2) / Integer.parseInt(valor2, 2);
+                break;
+        }
+        System.out.println("Resultado binário: " + Integer.toBinaryString(resultado));
+    }
+
+    public static boolean Valida(String valor, char[] valores){
+        for (char ch : valor.toCharArray()){
+            if (!String.valueOf(valores).contains(String.valueOf(ch))){
                 return false;
             }
         }
-    } 
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CodigoResolvidoA3 []";
+    }
 }
